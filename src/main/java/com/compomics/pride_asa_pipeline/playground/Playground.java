c47d107a2c9790f4016477cf1d4464e4dbf1ff27
kennyhelsens <kennyhelsens@3a48a7f6-d5c3-758a-6f63-b986e2e1a7fa>
2012-08-03 12:44:32 +0000

    - Added options to add spectrum.limit and spectrum.limit.size to the ExperimentServiceImpl. If spectrum.limit is set to TRUE, then only spectrum.limit.size MSMS spectra will be written to the MGF file. This feature can be used for debugging purposes, or to avoid crashing into projects with +1000000 Spectra.

--- src/main/java/com/compomics/pride_asa_pipeline/playground/Playground.java
+++ src/main/java/com/compomics/pride_asa_pipeline/playground/Playground.java
@@ -4,16 +4,12 @@
  */
 package com.compomics.pride_asa_pipeline.playground;
 
-import com.compomics.omssa.xsd.UserMod;
-import com.compomics.omssa.xsd.UserModCollection;
-import com.compomics.pride_asa_pipeline.model.Modification;
-import com.compomics.pride_asa_pipeline.modification.OmssaModiciationMarshaller;
-import com.compomics.pride_asa_pipeline.modification.impl.OmssaModificationMarshallerImpl;
 import com.compomics.pride_asa_pipeline.service.ExperimentService;
-import com.compomics.pride_asa_pipeline.service.ModificationService;
+import com.compomics.pride_asa_pipeline.service.SpectrumService;
 import com.compomics.pride_asa_pipeline.spring.ApplicationContextProvider;
 import org.springframework.context.ApplicationContext;
 
+import java.io.File;
 import java.util.Set;
 
 /**
@@ -25,33 +21,55 @@ public class Playground {
     public static void main(String[] args) {
         //load application context
         ApplicationContext applicationContext = ApplicationContextProvider.getInstance().getApplicationContext();
-        
-        ExperimentService experimentService = (ExperimentService) applicationContext.getBean("experimentService");
-
-        ModificationService modificationService = (ModificationService) applicationContext.getBean("modificationService");
-        Set<Modification> lModificationSet = modificationService.loadExperimentModifications(17183l);
-
-        System.out.println("printing pride-asap modfications");
-        for (Modification lNext : lModificationSet) {
-            System.out.println(String.format("%s - %s - %f", lNext.getName(), lNext.getLocation().toString(), lNext.getMassShift()));
-        }
-
-        System.out.println("marshalling into OMSSA xsd modfications");
-        OmssaModiciationMarshaller marshaller = new OmssaModificationMarshallerImpl();
-        UserModCollection lUserModCollection = marshaller.marshallModifications(lModificationSet);
-
-        System.out.println("printing omssa-xsd modfications");
-        for (Object lNext : lUserModCollection) {
-            UserMod lUserMod = (UserMod) lNext;
-            System.out.println(String.format("%s - %s - %f", lUserMod.getModificationName(), lUserMod.getLocation(), lUserMod.getMass()));
-        }
-
+//
+//
+//        ModificationService modificationService = (ModificationService) applicationContext.getBean("modificationService");
+//        Set<Modification> lModificationSet = modificationService.loadExperimentModifications(1748);
+//
+//        System.out.println("printing pride-asap modfications");
+//        for (Modification lNext : lModificationSet) {
+//            if(lNext != null){
+//                System.out.println(String.format("%s - %s - %f", lNext.getName(), lNext.getLocation().toString(), lNext.getMassShift()));
+//            }
+//        }
+//
+//        System.out.println("marshalling into OMSSA xsd modfications");
+//        OmssaModiciationMarshaller marshaller = new OmssaModificationMarshallerImpl();
+//        UserModCollection lUserModCollection = marshaller.marshallModifications(lModificationSet);
+//
+//        System.out.println("printing omssa-xsd modfications");
+//        for (Object lNext : lUserModCollection) {
+//            UserMod lUserMod = (UserMod) lNext;
+//            System.out.println(String.format("%s - %s - %f", lUserMod.getModificationName(), lUserMod.getLocation(), lUserMod.getMass()));
+//        }
+//
 
 //  modificationService.loadExperimentModifications(null);
 
 
+        ExperimentService experimentService;
+        experimentService = (ExperimentService) applicationContext.getBean("experimentService");
+        SpectrumService spectrumService = (SpectrumService) applicationContext.getBean("spectrumService");
 //        long numberOfPeptides = experimentService.getNumberOfPeptides("7662");
-//        File file = experimentService.getSpectraAsMgfFile("7662");
+
+//        File file = experimentService.getSpectraAsMgfFile("1748");
+
+        File file = experimentService.getSpectraAsMgfFile("7662");
+        System.out.println(file.getAbsolutePath());
+
+        Set<String> lAccessions = experimentService.getProteinAccessions("7662");
+        System.out.println(String.format("%d unique protein accessions", lAccessions.size()));
+
+//        List<Long> spectrumIds = Lists.newArrayList();
+//        spectrumIds.add(4190355l);
+//        spectrumIds.add(4190382l);
+//        spectrumIds.add(4190407l);
+//
+//
+//        spectrumService.cacheSpectra(spectrumIds);
+//        Map lCachedSpectrum = spectrumService.getCachedSpectrum(4190407l);
+//        System.out.println(String.format("spectrum has %d different peaks %s", lCachedSpectrum.keySet().size(), "blabla"));
+
 //        Set<String> proteinAccessions = experimentService.getProteinAccessions("7662");
 //
 //        PrideSpectrumAnnotator prideSpectrumAnnotator = (PrideSpectrumAnnotator) applicationContext.getBean("prideSpectrumAnnotator");
