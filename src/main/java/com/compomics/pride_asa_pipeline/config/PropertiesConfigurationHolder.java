366855e229fd9863ccc0cf3d4420e0ebb5a451ff
nielshulstaert <nielshulstaert@gmail.com>
2012-08-09 09:12:33 +0000

    changed holder: now using file constructor instead of string constructor

--- src/main/java/com/compomics/pride_asa_pipeline/config/PropertiesConfigurationHolder.java
+++ src/main/java/com/compomics/pride_asa_pipeline/config/PropertiesConfigurationHolder.java
@@ -1,41 +1,39 @@
 package com.compomics.pride_asa_pipeline.config;
 
+import com.compomics.pride_asa_pipeline.util.FileUtils;
+import java.io.File;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.log4j.Logger;
 
 /**
- * Created by IntelliJ IDEA.
- * User: niels
- * Date: 9/11/11
- * Time: 13:41
- * To change this template use File | Settings | File Templates.
+ * Created by IntelliJ IDEA. User: niels Date: 9/11/11 Time: 13:41 To change
+ * this template use File | Settings | File Templates.
  */
 public class PropertiesConfigurationHolder extends PropertiesConfiguration {
-    
+
     private static final Logger LOGGER = Logger.getLogger(PropertiesConfigurationHolder.class);
-    
     private static PropertiesConfigurationHolder ourInstance;
 
     static {
         try {
-            ourInstance = new PropertiesConfigurationHolder("resources/pride_asa_pipeline.properties");
+            File propertiesFile = FileUtils.getFileByRelativePath("resources/pride_asa_pipeline.properties");
+            ourInstance = new PropertiesConfigurationHolder(propertiesFile);
         } catch (ConfigurationException e) {
             LOGGER.error(e.getMessage(), e);
         }
     }
-    
+
     /**
      * Gets the PropertiesConfiguration instance
-     * 
-     * @return 
+     *
+     * @return
      */
     public static PropertiesConfigurationHolder getInstance() {
         return ourInstance;
     }
 
-    private PropertiesConfigurationHolder(String propertiesFileName) throws ConfigurationException {
-        super(propertiesFileName);
+    private PropertiesConfigurationHolder(File propertiesFile) throws ConfigurationException {
+        super(propertiesFile);
     }
-
 }
