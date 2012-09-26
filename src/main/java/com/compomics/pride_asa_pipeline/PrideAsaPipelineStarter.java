add41c54ea0e311ddb278f89048454003d02cdc6
nielshulstaert <nielshulstaert@gmail.com>
2012-09-26 10:27:21 +0000

    added log4j file appender

--- src/main/java/com/compomics/pride_asa_pipeline/PrideAsaPipelineStarter.java
+++ src/main/java/com/compomics/pride_asa_pipeline/PrideAsaPipelineStarter.java
@@ -4,6 +4,7 @@
  */
 package com.compomics.pride_asa_pipeline;
 
+import com.compomics.pride_asa_pipeline.config.PropertiesConfigurationHolder;
 import com.compomics.pride_asa_pipeline.gui.controller.MainController;
 import com.compomics.pride_asa_pipeline.spring.ApplicationContextProvider;
 import java.awt.EventQueue;
@@ -11,6 +12,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.logging.Level;
 import javax.swing.UIManager;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
@@ -23,33 +25,34 @@ import org.apache.commons.cli.ParseException;
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 
 /**
  *
  * @author Niels Hulstaert Hulstaert
  */
 public class PrideAsaPipelineStarter {
-    
-    private static final Logger LOGGER = Logger.getLogger(PrideAsaPipelineStarter.class);
+
+    //private static final Logger LOGGER = Logger.getLogger(PrideAsaPipelineStarter.class);
     private static final String HEADER = "[Pride automtic spectrum annotation pipeline]\n";
     private static final String USAGE = "java -jar <jar file name>";
     private static Options options;
-
+                
     /**
      * Main executable.
      *
      * @param commandLineArguments Commmand-line arguments.
      */
-    public static void main(String[] commandLineArguments) {
+    public static void main(String[] commandLineArguments) {           
         constructOptions();
-        
+
         displayBlankLines(1, System.out);
         displayHeader(System.out);
         displayBlankLines(2, System.out);
-        
         parse(commandLineArguments);
     }
-    
+
     public static void launchGuiMode() {
         /*
          * Set the Nimbus look and feel
@@ -63,18 +66,18 @@ public class PrideAsaPipelineStarter {
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());                    
+                    UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
         } catch (ClassNotFoundException ex) {
-            LOGGER.error(ex.getMessage(), ex);
+            //LOGGER.error(ex.getMessage(), ex);
         } catch (InstantiationException ex) {
-            LOGGER.error(ex.getMessage(), ex);
+            //LOGGER.error(ex.getMessage(), ex);
         } catch (IllegalAccessException ex) {
-            LOGGER.error(ex.getMessage(), ex);
+            //LOGGER.error(ex.getMessage(), ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            LOGGER.error(ex.getMessage(), ex);
+            //LOGGER.error(ex.getMessage(), ex);
         }
         //</editor-fold>
 
@@ -92,16 +95,16 @@ public class PrideAsaPipelineStarter {
             }
         });
     }
-    
+
     public static void launchCommandLineMode(String experimentAccession) {
-        LOGGER.debug("launching command line mode with experiment " + experimentAccession);
+        //LOGGER.debug("launching command line mode with experiment " + experimentAccession);
         ApplicationContext applicationContext = ApplicationContextProvider.getInstance().getApplicationContext();
         CommandLineRunner commandLineRunner = (CommandLineRunner) applicationContext.getBean("commandLineRunner");
         commandLineRunner.runPipeline(experimentAccession);
     }
-    
+
     public static void launchCommandLineMode(File experimentAccessionsFile) {
-        LOGGER.debug("launching command line mode with experiment accessions file " + experimentAccessionsFile.getAbsolutePath());
+        //LOGGER.debug("launching command line mode with experiment accessions file " + experimentAccessionsFile.getAbsolutePath());
         ApplicationContext applicationContext = ApplicationContextProvider.getInstance().getApplicationContext();
         CommandLineRunner commandLineRunner = (CommandLineRunner) applicationContext.getBean("commandLineRunner");
         commandLineRunner.runPipeline(experimentAccessionsFile);
@@ -131,7 +134,7 @@ public class PrideAsaPipelineStarter {
             if (commandLine.hasOption('f')) {
                 String experimentAccessionsFilePath = commandLine.getOptionValue('f');
                 File experimentAccessionsFile = new File(experimentAccessionsFilePath);
-                if (experimentAccessionsFile.exists()) {                    
+                if (experimentAccessionsFile.exists()) {
                     launchCommandLineMode(experimentAccessionsFile);
                 } else {
                     System.out.println("No file with path \"" + experimentAccessionsFilePath + "\" could be found.");
@@ -160,10 +163,10 @@ public class PrideAsaPipelineStarter {
      */
     private static void constructOptions() {
         options = new Options();
-        
+
         options.addOption("h", "help", Boolean.FALSE, "Help");
         options.addOption("u", "usage", Boolean.FALSE, "Usage");
-        
+
         Option accessionStringOption = new Option("a", "accession", Boolean.TRUE, "Experiment accession");
         accessionStringOption.setArgName("accession");
         Option accessionFileOption = new Option("f", "accessions_file", Boolean.TRUE, "Experiment accessions file path");
@@ -171,7 +174,7 @@ public class PrideAsaPipelineStarter {
         OptionGroup commandLineModeOptionGroup = new OptionGroup();
         commandLineModeOptionGroup.addOption(accessionFileOption);
         commandLineModeOptionGroup.addOption(accessionStringOption);
-        
+
         options.addOptionGroup(commandLineModeOptionGroup);
     }
 
