de639b1c4b9c826b0b2b22f022e8f1c06eb01c56
nielshulstaert@gmail.com <nielshulstaert@gmail.com@3a48a7f6-d5c3-758a-6f63-b986e2e1a7fa>
2013-10-11 11:17:44 +0000

    increased version of PrideXmlValidator

--- trunk/src/main/java/com/compomics/pride_asa_pipeline/playground/Playground.java
+++ trunk/src/main/java/com/compomics/pride_asa_pipeline/playground/Playground.java
@@ -8,7 +8,9 @@ import com.compomics.pride_asa_pipeline.PrideXmlCommandLineRunner;
 import com.compomics.pride_asa_pipeline.logic.PrideXmlSpectrumAnnotator;
 import com.compomics.pride_asa_pipeline.model.Modification;
 import com.compomics.pride_asa_pipeline.model.SpectrumAnnotatorResult;
+import com.compomics.pride_asa_pipeline.service.ExperimentService;
 import com.compomics.pride_asa_pipeline.service.ModificationService;
+import com.compomics.pride_asa_pipeline.service.PrideXmlExperimentService;
 import com.compomics.pride_asa_pipeline.service.PrideXmlModificationService;
 import com.compomics.pride_asa_pipeline.spring.ApplicationContextProvider;
 import java.io.BufferedReader;
@@ -39,12 +41,16 @@ public class Playground {
         ApplicationContext applicationContext = ApplicationContextProvider.getInstance().getApplicationContext();        
 
         PrideXmlCommandLineRunner prideXmlCommandLineRunner = applicationContext.getBean("prideXmlCommandLineRunner", PrideXmlCommandLineRunner.class);
-        prideXmlCommandLineRunner.runPrideXmlPipeline(new File("C:\\Users\\niels\\Desktop\\PRIDE_Exp_Complete_Ac_15346.xml"), true);
+        prideXmlCommandLineRunner.runPrideXmlPipeline(new File("C:\\Users\\niels\\Desktop\\ExampleDataSets\\PRIDE_Exp_Complete_Ac_11954.xml"), true);
         
         SpectrumAnnotatorResult spectrumAnnotatorResult = prideXmlCommandLineRunner.getPrideXmlSpectrumAnnotator().getSpectrumAnnotatorResult();
         ModificationService modificationService = prideXmlCommandLineRunner.getPrideXmlSpectrumAnnotator().getModificationService();
         Map<Modification, Integer> usedModifications = modificationService.getUsedModifications(spectrumAnnotatorResult);
         Map<Modification, Double> estimateModificationRate = modificationService.estimateModificationRate(usedModifications, spectrumAnnotatorResult, 0.05);  
+        
+//        PrideXmlSpectrumAnnotator prideXmlSpectrumAnnotator = (PrideXmlSpectrumAnnotator) applicationContext.getBean("prideXmlSpectrumAnnotator");
+//        PrideXmlExperimentService iPrideService = prideXmlSpectrumAnnotator.getExperimentService();
+//        iPrideService.init(new File("C:\\Users\\niels\\Desktop\\ExampleDataSets\\PRIDE_Exp_Complete_Ac_15346.xml"));
         
         System.out.println("test");
     }
