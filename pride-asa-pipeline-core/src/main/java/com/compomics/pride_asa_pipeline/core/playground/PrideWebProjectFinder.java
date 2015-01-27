a54040b44ccb0f3a5e666208b86c8a65f55d3b06
Kenneth.verheggen <kenneth.verheggen@gmail.com>
2015-01-27 10:34:24 +0000

    Adopted ReShake prideXML-> MGF  conversion strategy for all parsers

--- pride-asa-pipeline-core/src/main/java/com/compomics/pride_asa_pipeline/core/playground/PrideWebProjectFinder.java
+++ pride-asa-pipeline-core/src/main/java/com/compomics/pride_asa_pipeline/core/playground/PrideWebProjectFinder.java
@@ -33,8 +33,8 @@ public class PrideWebProjectFinder {
 
     public static void main(String[] args) throws IOException, ParseException {
 
-        //String projectAccession = "PXD001423";
-        String projectAccession = "PRD000214";
+        String projectAccession = "PXD001423";
+        //String projectAccession = "PRD000214";
         PrideWebUtils prideService = new PrideWebUtils();
         LOGGER.info("Getting metadata for " + projectAccession);
         PrideProject aProject = prideService.getPrideProject(projectAccession);
