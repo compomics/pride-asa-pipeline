2a4a43674ab1b2fd042553e77713a63436671887
Kenneth Verheggen <kenneth.verheggen@gmail.com>
2015-11-06 16:03:50 +0100

    added parameter extraction exception

---
+++ pride-asa-pipeline-model/src/main/java/com/compomics/pride_asa_pipeline/core/exceptions/ParameterExtractionException.java
@@ -0,0 +1,12 @@
+package com.compomics.pride_asa_pipeline.core.exceptions;
+
+/**
+ *
+ * @author Kenneth Verheggen
+ */
+public class ParameterExtractionException extends Exception {
+
+    public ParameterExtractionException(String message) {
+        super(message);
+    }
+}
